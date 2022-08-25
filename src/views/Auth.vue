3ce3705547ae0eddc1025da6b41861cda36ff42a
Miquel Mayordomo <miquelmayordomo@gmail.com>
2022-08-25 10:01:34 +0200

    vercel .json added

--- src/views/Auth.vue
+++ src/views/Auth.vue
@@ -1,10 +1,13 @@
 <template>
-  <div class="flex flex-row">
-    <div>
+
+    <div class="container m-auto"> 
       <router-view></router-view>
     </div>
-    <img src="https://images.unsplash.com/photo-1626757431530-7a28db690a5c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=882&q=80" alt="" >
-  </div>
+
+    <div>
+      <img src="https://images.unsplash.com/photo-1626757431530-7a28db690a5c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=882&q=80" alt="" >
+    </div>
+
 </template>
 
 <script setup></script>
